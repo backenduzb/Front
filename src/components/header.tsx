e830d555578f64a67fdf96363ac735769074ebac
backenduzb <qobiljonsharipov33@gmail.com>
2025-07-15 21:50:17 +0500

    csac

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import ImageLogo from "../assets/img/image.png";
 import { Link } from "react-router-dom";
 import { useTranslation } from "react-i18next";
+import { Menu, X } from "lucide-react";
 
 
 export default function Header() {
@@ -111,7 +112,12 @@ export default function Header() {
               <path d="M6 .278a.77.77 0 0 1 .08.858 7.2 7.2 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277q.792-.001 1.533-.16a.79.79 0 0 1 .81.316.73.73 0 0 1-.031.893A8.35 8.35 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.75.75 0 0 1 6 .278" />
             </svg>}
           </button>
-         
+          <button
+            onClick={() => setMenuOpen(!menuOpen)}
+            className="text-gray-800 dark:text-white"
+          >
+            {menuOpen ? <X/> : <Menu />}
+          </button>
         </div>
       </div>
       {/* Mobile menu */}
